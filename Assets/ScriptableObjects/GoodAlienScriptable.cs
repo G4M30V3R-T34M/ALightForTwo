f1faaeb76984beb2b46580bce5caeef46f914a19
Neengash <pepmanas93@gmail.com>
2022-01-29 09:45:39 +0100

    Renamed AlienScriptable to GoodAlienScriptable

---
+++ Assets/ScriptableObjects/GoodAlienScriptable.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GoodAlienScriptable", menuName = "Scriptables/GoodAlienScriptable", order = 0)]
+public class GoodAlienScriptable : ScriptableObject
+{
+    [Range(0,10)]
+    public float Speed;
+
+    [Range(1,5)]
+    public int Health;
+
+    [Range(0, 5)]
+    public float interactDistance;
+}
