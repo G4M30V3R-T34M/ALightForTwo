6f010d571d222b415ea39575ccc05a53364212df
Jaume Riera <jaumerg95@gmail.com>
2022-01-29 09:12:12 +0100

    Format astronaut controller script

--- Assets/Scripts/Astronaut/AstronautController.cs
+++ Assets/Scripts/Astronaut/AstronautController.cs
@@ -7,18 +7,15 @@ public class AstronautController : MonoBehaviour
     [SerializeField] private float velocity;
     [SerializeField] private ObjectPool op;
     
-    void Start()
-    {
+    void Start() {
         op = gameObject.GetComponent<ObjectPool>();
     }
 
-    void Update()
-    {
+    void Update() {
         UpdatePosition();
     }
 
-    private void UpdatePosition()
-    {
+    private void UpdatePosition() {
         float positionX = this.transform.position.x + (Input.GetAxisRaw("Horizontal") * this.velocity * Time.deltaTime);
         float positionY = this.transform.position.y + (Input.GetAxisRaw("Vertical") * this.velocity * Time.deltaTime);
         this.transform.position = new Vector2(positionX, positionY);
