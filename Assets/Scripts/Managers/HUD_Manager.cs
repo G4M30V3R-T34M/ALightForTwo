fb98a0f38f928437b9bf893631ffea8a5142fbd2
Neengash <pepmanas93@gmail.com>
2022-05-01 17:04:13 +0200

    Added game clear sfx

--- Assets/Scripts/Managers/HUD_Manager.cs
+++ Assets/Scripts/Managers/HUD_Manager.cs
@@ -2,11 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class HUD_Manager : MonoBehaviour
 {
     [SerializeField] GameOverManager GO_Manager;
     [SerializeField] GameObject[] fuel = new GameObject[3];
+    AudioSource audioSource;
     private int index;
+
+    private void Awake() {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     void Start()
     {
         foreach (GameObject element in fuel) {
@@ -20,6 +27,7 @@ public class HUD_Manager : MonoBehaviour
         fuel[index].SetActive(true);
         index++;
         if (index == fuel.Length) {
+            audioSource.Play();
             GO_Manager.GameOver();
         }
     }
